059e116a6eac7333c6a1c34b5cab662de9aa9723
LuizGarbini <garbasneto@gmail.com>
2024-04-19 00:55:38 -0300

    Structuring the countdown

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -1,18 +1,19 @@
-import state from './state.js'
+import state from './state.js';
+import * as timer from './timer.js';
 
 export function toggleRunning() {
-  state.isRunning = document.documentElement.classList.toggle('running')
+  state.isRunning = document.documentElement.classList.toggle('running');
+
+  timer.countdown();
 }
 
 export function reset() {
-    state.isRunning = false
-    document.documentElement.classList.remove('running')
+  state.isRunning = false;
+  document.documentElement.classList.remove('running');
 }
 
-export function set() {
-
-}
+export function set() {}
 
 export function toggleMusic() {
-    state.isMute = document.documentElement.classList.toggle('music-on')
+  state.isMute = document.documentElement.classList.toggle('music-on');
 }
